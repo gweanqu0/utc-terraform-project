f866e5d503fae33afcd7237404919bc2214a4315
takoumenecarel-gh <takoumenecarel@icloud.com>
2026-02-18 21:15:59 -0700

    first commit

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "utc-app-stepin.ink.20260218 "
+    bucket = "multi-azs-terraform"
     key = "dev/terraform.tfstate"
     region = "us-east-1"
     use_lockfile = true
