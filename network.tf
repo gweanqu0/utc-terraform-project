97357d3a06d35e4383d054a974016ffb921cdf7f
takoumenecarel-gh <takoumenecarel@icloud.com>
2026-02-18 21:31:39 -0700

    first commit

--- network.tf
+++ network.tf
@@ -1,189 +1,177 @@
 resource "aws_vpc" "utc_vpc" {
   cidr_block = "10.10.0.0/16"
+  region = "us-east-1"
   instance_tenancy = "default"
   enable_dns_hostnames = true
   enable_dns_support = true
-  tags = {
-    Name = "utc-app"
-  }
 }
-
-# Create IGW Internet Gateway
-resource "aws_internet_gateway" "utc_igw" {
-  vpc_id = aws_vpc.utc_vpc.id
-}
-
-# Public Subnets (NAT lives here)
-resource "aws_subnet" "public1" {
+resource "aws_subnet" "private1" {
   vpc_id     = aws_vpc.utc_vpc.id
   cidr_block = "10.10.1.0/24"
   availability_zone = "us-east-1a"
   tags = {
-    Name = "subnet-public1"
-  }
-}
-resource "aws_subnet" "public2" {
-  vpc_id     = aws_vpc.utc_vpc.id
-  cidr_block = "10.10.2.0/24"
-  availability_zone = "us-east-1b"
-  tags = {
-    Name = "subnet-public2"
-  }
-}
-resource "aws_subnet" "public3" {
-  vpc_id     = aws_vpc.utc_vpc.id
-  cidr_block = "10.10.3.0/24"
-  availability_zone = "us-east-1c"
-    tags = {
-    Name = "subnet-public3"
-  }
-}
-
-# Private Subnets
-
-resource "aws_subnet" "private1" {
-  vpc_id     = aws_vpc.utc_vpc.id
-  cidr_block = "10.10.4.0/24"
-  availability_zone = "us-east-1a"
-  tags = {
-    Name = "subnet-private1"
+    Name = "subnet_private1"
   }
 }
 resource "aws_subnet" "private2" {
   vpc_id     = aws_vpc.utc_vpc.id
-  cidr_block = "10.10.5.0/24"
+  cidr_block = "10.10.2.0/24"
   availability_zone = "us-east-1a"
   tags = {
-    Name = "subnet-private2"
+    Name = "subnet_private2"
   }
 }
 resource "aws_subnet" "private3" {
   vpc_id     = aws_vpc.utc_vpc.id
-  cidr_block = "10.10.6.0/24"
+  cidr_block = "10.10.3.0/24"
   availability_zone = "us-east-1b"
   tags = {
-    Name = "subnet-private3"
+    Name = "subnet_private3"
   }
 }
 resource "aws_subnet" "private4" {
   vpc_id     = aws_vpc.utc_vpc.id
-  cidr_block = "10.10.7.0/24"
+  cidr_block = "10.10.4.0/24"
   availability_zone = "us-east-1b"
   tags = {
-    Name = "subnet-private4"
+    Name = "subnet_private4"
   }
 }
+
 resource "aws_subnet" "private5" {
   vpc_id     = aws_vpc.utc_vpc.id
-  cidr_block = "10.10.8.0/24"
+  cidr_block = "10.10.5.0/24"
   availability_zone = "us-east-1c"
   tags = {
-    Name = "subnet-private5"
+    Name = "subnet_private5"
   }
 }
 resource "aws_subnet" "private6" {
   vpc_id     = aws_vpc.utc_vpc.id
-  cidr_block = "10.10.9.0/24"
+  cidr_block = "10.10.6.0/24"
   availability_zone = "us-east-1c"
   tags = {
-    Name = "subnet-private6"
+    Name = "subnet_private6"
   }
 }
-
-# Create eip Elastics IP (One per NAT)
-resource "aws_eip" "nat_eip1" { domain = "vpc"}
-resource "aws_eip" "nat_eip2" { domain = "vpc"}
-resource "aws_eip" "nat_eip3" { domain = "vpc"}
-
-# create NAt gateway (One per AZ)
+resource "aws_subnet" "public1" {
+  vpc_id     = aws_vpc.utc_vpc.id
+  cidr_block = "10.10.7.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "subnet_public1"
+  }
+}
+resource "aws_subnet" "public2" {
+  vpc_id     = aws_vpc.utc_vpc.id
+  cidr_block = "10.10.8.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "subnet_public2"
+  }
+}
+resource "aws_subnet" "public3" {
+  vpc_id     = aws_vpc.utc_vpc.id
+  cidr_block = "10.10.9.0/24"
+  availability_zone = "us-east-1c"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "subnet_public3"
+  }
+}
+#create EIP
+resource "aws_eip" "utc-eip-1" {
+}
+resource "aws_eip" "utc-eip-2" {
+}
+resource "aws_eip" "utc-eip-3" {
+}
+#create NAT gateway
 resource "aws_nat_gateway" "NAT1" {
-  allocation_id = aws_eip.nat_eip1.id
+  allocation_id = aws_eip.utc-eip-1.id
   subnet_id     = aws_subnet.public1.id
-  depends_on = [aws_internet_gateway.utc_igw]
 }
 resource "aws_nat_gateway" "NAT2" {
-  allocation_id = aws_eip.nat_eip2.id
-  subnet_id     = aws_subnet.private2.id
-  depends_on = [aws_internet_gateway.utc_igw]
+  allocation_id = aws_eip.utc-eip-2.id
+  subnet_id     = aws_subnet.public2.id
+  
 }
 resource "aws_nat_gateway" "NAT3" {
-  allocation_id = aws_eip.nat_eip3.id
-  subnet_id     = aws_subnet.private3.id
-  depends_on = [aws_internet_gateway.utc_igw]
+  allocation_id = aws_eip.utc-eip-3.id
+  subnet_id     = aws_subnet.public3.id
+}
+ #create iGW
+resource "aws_internet_gateway" "utc-igw" {
+  vpc_id = aws_vpc.utc_vpc.id
 }
 
-# Public Route Table (only one)
+# Public Route Table - 1 needed for the Internet gateway Public
 resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.utc_vpc.id
+  vpc_id = aws_vpc.utc_vpc.id   
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.utc_igw.id
+    gateway_id = aws_internet_gateway.utc-igw.id
+  }
+}
+# Private Route Tables - 2 needed for the NAT gateways
+resource "aws_route_table" "private_1" {
+  vpc_id = aws_vpc.utc_vpc.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.NAT1.id
   }
 }
 
-# Private Route Table ( 1 per AZ)
-resource "aws_route_table" "private_rt1" {
+resource "aws_route_table" "private_2" {
   vpc_id = aws_vpc.utc_vpc.id
   route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.NAT1.id
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.NAT2.id
   }
 }
-resource "aws_route_table" "private_rt2" {
+resource "aws_route_table" "private_3" {
   vpc_id = aws_vpc.utc_vpc.id
   route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.NAT2.id
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.NAT3.id
   }
 }
-
-resource "aws_route_table" "private_rt3" {
-  vpc_id = aws_vpc.utc_vpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.NAT3.id
-  }
-}
-
-
 # Route Table Associations
-
-#public
 resource "aws_route_table_association" "public_1" {
-  subnet_id = aws_subnet.public1.id
+  subnet_id      = aws_subnet.public1.id
   route_table_id = aws_route_table.public.id
 }
 resource "aws_route_table_association" "public_2" {
-  subnet_id = aws_subnet.public2.id
+  subnet_id      = aws_subnet.public2.id
   route_table_id = aws_route_table.public.id
 }
 resource "aws_route_table_association" "public_3" {
-  subnet_id = aws_subnet.public3.id
+  subnet_id      = aws_subnet.public3.id
   route_table_id = aws_route_table.public.id
 }
-
-# Private
 resource "aws_route_table_association" "private_1" {
-  subnet_id = aws_subnet.private1.id
-  route_table_id = aws_route_table.private_rt1.id
+  subnet_id      = aws_subnet.private1.id
+  route_table_id = aws_route_table.private_1.id
 }
 resource "aws_route_table_association" "private_2" {
-  subnet_id = aws_subnet.private2.id
-  route_table_id = aws_route_table.private_rt1.id
+  subnet_id      = aws_subnet.private2.id
+  route_table_id = aws_route_table.private_1.id
 }
 resource "aws_route_table_association" "private_3" {
-  subnet_id = aws_subnet.private3.id
-  route_table_id = aws_route_table.private_rt1.id
+  subnet_id      = aws_subnet.private3.id
+  route_table_id = aws_route_table.private_2.id
 }
 resource "aws_route_table_association" "private_4" {
-  subnet_id = aws_subnet.private4.id
-  route_table_id = aws_route_table.private_rt2.id
+  subnet_id      = aws_subnet.private4.id
+  route_table_id = aws_route_table.private_2.id
 }
 resource "aws_route_table_association" "private_5" {
-  subnet_id = aws_subnet.private5.id
-  route_table_id = aws_route_table.private_rt3.id
+  subnet_id      = aws_subnet.private5.id
+  route_table_id = aws_route_table.private_3.id
 }
 resource "aws_route_table_association" "private_6" {
-  subnet_id = aws_subnet.private6.id
-  route_table_id = aws_route_table.private_rt3.id
+  subnet_id      = aws_subnet.private6.id
+  route_table_id = aws_route_table.private_3.id
 }
