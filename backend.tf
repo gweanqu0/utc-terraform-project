12e6867bc70bccb5eb0f5b2ee3f6a94aaa399131
gweanqu0 <gweanquoi9@gmail.com>
2026-02-19 21:18:02 -0500

    terra

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "multi-azs-terraform"
+    bucket = "week7-terraform-bucket"
     key = "dev/terraform.tfstate"
     region = "us-east-1"
     use_lockfile = true
